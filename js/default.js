05395394ff1aa7c5c6fc27ff3a4136bbd73860d2
soisausem <38421092+soisausem@users.noreply.github.com>
2018-11-19 13:58:51 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -32,6 +32,48 @@ document.getElementById('countdown').textContent = '現在' + counter[5] + '年'
 refresh();
 }
 
+function setCookie(c_name,value,expiredays){
+	var extime = new Date().getTime();
+	var cltime = new Date(extime + (60*60*24*1000*expiredays));
+	var exdate = cltime.toUTCString();
+	
+	var s="";
+	s += c_name +"="+ escape(value);
+	s += "; path="+ location.pathname;
+	if(expiredays){
+		s += "; expires=" +exdate"; ";
+	}else{
+		s += "; ";
+	}
+	
+	document.cookie=s;
+}
+
+function getCookie(c_name){
+	var st="";
+	var ed="";
+	if(0 < document.cookie.length){
+		st=document.cookie.index(c_name + "=");
+		if(st!=-1){
+			st=st+c_name.length+1;
+			ed=document.cookie.indexOf(";",st);
+			if(ed==-1) ed=document.cookie.length;
+			return unescape(document.cookie.substring(st,ed));
+		}
+	}
+	return "";
+}
+
+var last_date = getCookie('lastDate');
+if(last_date){
+	document.getElementById('cookie').textContent = '前回訪れた時間:' + last_date;
+}else{
+	document.getElementById('cookie').textContent = 'はじめまして';
+}
+
+var current_time = new Date();
+setCookie('lastDate', current_time.toString(), 7);
+
 var refresh = function(){
 	setTimeout(update, 1000);
 }
