25b34b1d7b9f232122d8d071fe9f591d5ea13b73
soisausem <38421092+soisausem@users.noreply.github.com>
2018-11-19 13:41:54 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -10,8 +10,22 @@ var separate_time = function(time){
 
 var update = function(){
 var now = new Date();
-var counter = separate_time(now);
-  document.getElementById('form').select.onchange = function(){
+var counter = separate_time(now); 
+	
+	function getFileName(){
+		return window.location.href.split('/').pop();
+	}
+	
+	var filename = getFileName();
+	var opt;
+	if(filename === 'other.html'){
+		opt = document.querySelector('option[value="other.html"]');
+	}else{
+		opt = document.querySelector('option[value="index.html"]');
+	}
+	opt.selected = true;
+	
+document.getElementById('form').select.onchange = function(){
     location.href = document.getElementById('form').select.value;
   }
 document.getElementById('countdown').textContent = '現在' + counter[5] + '年' +counter[4] + '月' +counter[3] + '日' +counter[2] + '時' +counter[1] + '分' +counter[0] + '秒' ;
